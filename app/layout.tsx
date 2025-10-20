5953e7fd2536f1c28be0de5b6c8dec878b4b0ad9
rauf322 <rauf.faizov.200271@gmail.com>
2025-10-20 23:49:40 +0200

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '../assets/styles/globals.css';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+
+export const metadata: Metadata = {
+  title: 'storeAI',
+  description: 'A modern built-in AI store',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
