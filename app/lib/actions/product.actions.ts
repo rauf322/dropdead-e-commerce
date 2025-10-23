ec79e063feba99401948cceb15a4f26d0b765c0e
rauf322 <rauf.faizov.200271@gmail.com>
2025-10-24 01:37:19 +0200

    set up middleware between client and prisma

--- app/lib/actions/product.actions.ts
+++ app/lib/actions/product.actions.ts
@@ -1,21 +1,15 @@
 'use server';
 
-import { PrismaClient } from '@prisma/client';
 import { convertToPlainObject } from '../utils';
+import { prisma } from '@/db/prisma';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 import { Product } from '@/app/types';
 
 export async function getLatestProducts(): Promise<Product[]> {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: 'desc' },
   });
 
-  return data.map((product: { price: { toString: () => string }; rating: { toString: () => string } }) => ({
-    ...convertToPlainObject(product),
-    price: product.price.toString(),
-    rating: product.rating.toString(),
-  })) as Product[];
+  return convertToPlainObject(data);
 }
