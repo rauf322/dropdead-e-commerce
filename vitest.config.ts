cb8d3c9e384b61a9406672076a7a91c34286309b
rauf322 <rauf.faizov.200271@gmail.com>
2025-11-03 03:08:25 +0100

    prevent vitest to run e2e tests
    q
    
    q
    
    q
    
    q
    
    q

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [react()],
@@ -8,14 +8,15 @@ export default defineConfig({
     environment: 'happy-dom',
     globals: true,
     setupFiles: ['./vitest.setup.ts'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/e2e/**']
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@/db': path.resolve(__dirname, './db'),
-    },
+      '@/db': path.resolve(__dirname, './db')
+    }
   },
   server: {
-    open: true,
-  },
-});
+    open: true
+  }
+})
