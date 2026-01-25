b13da59fd658d9fa8cfea1930a91ba58bfa184d0
Rauf <82578227+rauf322@users.noreply.github.com>
2026-01-25 20:53:23 +0100

    fix:self referencing in zod schema

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -70,7 +70,7 @@ export const signUpFormShema = z
     password: z.string().min(6, 'Password must be at least 6 characters long'),
     confirmPassword: z.string().min(6, 'Confirm password must be at least 6 characters long')
   })
-  .refine((data: z.infer<typeof signUpFormShema>) => data.password == data.confirmPassword, {
+  .refine(data => data.password === data.confirmPassword, {
     message: "Passwords don't match",
     path: ['confirmPassword']
   })
